dc111bfcdee2be4010623455783b62d115911254
7camc7 <108956844+7camc7@users.noreply.github.com>
2022-11-27 09:37:41 -0500

    updated photos

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ from functools import wraps
 from sqlalchemy import Table, Column, Integer, ForeignKey
 import os
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
